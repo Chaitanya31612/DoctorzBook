8d91a17ee138709215ecd5aa57c92e0b597f51df
kashishbhandula <kashishbhandula112@gmail.com>
2022-04-30 10:11:09 +0530

    User model updated with some extra fields

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,11 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true, min: 6, max: 255 },
   email: { type: String, required: true, min: 6, max: 255 },
   password: { type: String, required: true, max: 1024, min: 5 },
+  qualification:{ type: String, required: true, max: 1024, min: 5 },
+  fee:{ type:Number, required: true},
+  
+
+
   profilePhoto: {
     type: String,
     default:
